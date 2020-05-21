66d2b19c972d9cc7b97fd0124cd051a83605ad70
Quill McConnell <quill8197@yahoo.com>
2020-05-20 21:25:47 -0400

    Added a survey view with a form

--- index.php
+++ index.php
@@ -9,11 +9,22 @@ require_once('vendor/autoload.php');
 //Instantiate the F3 Base class
 $f3 = Base::instance();
 
+//Define arrays
+$f3->set('midtermOptions', array('I need groceries', 'Social distance your way out of my conversation',
+    'Today is Midterm Day'));
+
 //Define default route
 $f3->route('GET /', function ()
 {
     echo "<h1>Midterm Survey</h1>";
     echo "<a href='survey'>Take my Midterm Survey</a>";
+});
+
+$f3->route('GET|POST /survey', function ()
+{
+    //Display order form
+    $view = new Template();
+    echo $view->render('views/survey.html');
 });
 
 //Run fat free last
